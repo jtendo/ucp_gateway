033c3edd1eaf60a819a818f98a79d2039caf6722
Lepiocha <butcherek@gmail.com>
2011-11-15 10:49:41 +0100

    Concatenation added

--- src/ucp_messages.erl
+++ src/ucp_messages.erl
@@ -3,6 +3,7 @@
 -author('andrzej.trawinski@jtendo.com').
 
 -include("ucp_syntax.hrl").
+-include("logger.hrl").
 
 -export([
          create_cmd_51_text/4,
@@ -12,21 +13,173 @@
          create_cmd_60/3,
          create_cmd_31/2,
          create_ack/1,
+         split_message/3,
          analyze_message/1]).
 
--compile([debug_info]).
+%-ifdef(TEST).
+-compile([export_all]).
+%-endif.
 
--define(BODY_LEN, 153).
--define(HEADER_LEN, 14).
--define(EMPTY_BODY_LEN_51, 40).
--define(EMPTY_BODY_LEN_60, 18).
--define(EMPTY_BODY_LEN_31, 8).
 -define(BIN_BODY_MAX_SIZE, 140).
+-define(UDH_CONCAT_IE_SIZE, 5).
+-define(STK_IE, 16#70).
 
 
 %% ---------------------------------------------------------------------
 %% API
 %% ---------------------------------------------------------------------
+
+split_message(Message, XSer, CNTR) when is_binary(Message) ->
+    % TODO: catch parsing errors
+    {ok, Services} = xser_to_services(XSer),
+    {ok, L} = service_to_udh(proplists:get_value(1, Services)),
+    % Remove IE: 00 (Concatenated short messages IE) if exists
+    UDH = proplists:delete(0, L),
+    % Check - do we need splitting
+    UDHL = get_udh_size(UDH),
+    case ((size(Message) + UDHL) > ?BIN_BODY_MAX_SIZE) of
+        true ->
+            case do_splitting(Message, UDH) of
+                {ok, Result} ->
+                    % pass services without UDH - we'll build it inside
+                    add_concat_info(Result, proplists:delete(1, Services), UDH, CNTR);
+                Error ->
+                    Error
+            end;
+        _ ->
+            % splitting not needed
+            ModXSer = case UDHL of
+                         0 -> XSer;
+                         _ ->
+                            {ok, Service1} = udh_to_service(UDH),
+                            {ok, NewXSer} = services_to_xser([Service1]),
+                            NewXSer
+                      end,
+            {result, [{ModXSer, Message}]}
+    end;
+
+split_message(Message, XSer, CNTR) when is_list(Message) ->
+    BinMessage = list_to_binary(Message),
+    split_message(BinMessage, XSer, CNTR).
+
+
+add_concat_info(Bins, Services, UDH, CNTR) ->
+    add_concat_info(Bins, Services, UDH, CNTR, length(Bins), 1, []).
+
+add_concat_info([Bin], Services, UDH, _CNTR, Parts, _PartNo, _Result) when Parts =< 1 ->
+    {ok, Service1} = udh_to_service(UDH),
+    {ok, XSer} = services_to_xser([Service1 | Services]),
+    {result, {XSer, Bin}};
+add_concat_info([], _Services, _UDH, _CNTR, _Parts, _PartNo, Result) ->
+    {result, lists:reverse(Result)};
+add_concat_info([H|T], Services, UDH, CNTR, Parts, PartNo, Result) ->
+    Ref = CNTR rem 256,
+    IE = {0, {info_element, {0, 3, [Ref, Parts, PartNo]}}},
+    {ok, Service1} = udh_to_service([IE | UDH]),
+    {ok, XSer} = services_to_xser([Service1 | Services]),
+    ModUDH = case PartNo of
+                1 -> proplists:delete(?STK_IE, UDH);
+                _ -> UDH
+             end,
+    add_concat_info(T, Services, ModUDH, CNTR, Parts, PartNo+1, [{XSer, H}|Result]).
+
+%%--------------------------------------------------------------------
+%% Perform message splitting
+%%--------------------------------------------------------------------
+do_splitting(Message, UDH) ->
+    do_splitting(Message, UDH, []).
+
+do_splitting(_, _, Result) when length(Result) > 4 ->
+    {error, message_too_long};
+do_splitting([], _, Result) ->
+    {ok, lists:reverse(Result)};
+do_splitting(Message, UDH, []) ->
+    % Handle first part
+    UDHL = get_udh_size(UDH),
+    case ((size(Message) + UDHL + ?UDH_CONCAT_IE_SIZE) > ?BIN_BODY_MAX_SIZE) of
+        true ->
+            MaxSize = ?BIN_BODY_MAX_SIZE - UDHL - ?UDH_CONCAT_IE_SIZE,
+            <<Part:MaxSize/binary, Rest/binary>> = Message,
+            % HACK: remove STK header if exists (70)
+            ModUDH = proplists:delete(?STK_IE, UDH),
+            do_splitting(Rest, ModUDH, [Part]);
+        _ ->
+            do_splitting([], UDH, [Message])
+    end;
+do_splitting(Message, UDH, Result) ->
+    % Handle following parts
+    UDHL = get_udh_size(UDH), % +1 for UDHL field
+    case ((size(Message) + UDHL + ?UDH_CONCAT_IE_SIZE) > ?BIN_BODY_MAX_SIZE) of
+        true ->
+            MaxSize = ?BIN_BODY_MAX_SIZE - UDHL - ?UDH_CONCAT_IE_SIZE,
+            <<Part:MaxSize/binary, Rest/binary>> = Message,
+            do_splitting(Rest, UDH, [Part | Result]);
+        _ ->
+            do_splitting([], UDH, [Message | Result])
+    end.
+
+%%--------------------------------------------------------------------
+%% Translating XSer string into services and vice versa
+%%--------------------------------------------------------------------
+xser_to_services(XSer) ->
+    % transform hexstr to list
+    xser_to_services(hex:hexstr_to_list(XSer), []).
+
+xser_to_services([], Acc) ->
+    {ok, Acc};
+xser_to_services([Type, Len | Tail], Acc) ->
+    Data = lists:sublist(Tail, Len),
+    Rest = lists:nthtail(Len, Tail),
+    xser_to_services(Rest, [{Type, {service, {Type, Len, Data}}} | Acc]).
+
+services_to_xser(L) ->
+    services_to_xser(L, []).
+
+services_to_xser([], Result) ->
+    {ok, lists:flatten(hex:to_hexstr(Result))};
+services_to_xser([{Type, {service, {Type, Len, Data}}} | Rest], Result) ->
+    services_to_xser(Rest, [Type, Len, Data | Result]).
+
+%%--------------------------------------------------------------------
+%% Translating services to udh elements and vice versa
+%%--------------------------------------------------------------------
+service_to_udh({service, {1, _, [_Len | Data]}}) -> % cut off UDHL
+    service_to_udh(Data, []);
+service_to_udh(_) ->
+    {ok, []}.
+
+service_to_udh([], Acc) ->
+    {ok, Acc};
+service_to_udh([IEI, IEILen | Tail], Acc) ->
+    IEIData = lists:sublist(Tail, IEILen),
+    Rest = lists:nthtail(IEILen, Tail),
+    service_to_udh(Rest, [{IEI, {info_element, {IEI, IEILen, IEIData}}} | Acc]).
+
+udh_to_service([]) ->
+    {ok, []};
+udh_to_service(L) ->
+    udh_to_service(L, []).
+
+udh_to_service([], Result) ->
+    Data = lists:flatten(Result),
+    Len = length(Data),
+    {ok, {1, {service, {1, Len+1, [Len | Data]}}}};
+udh_to_service([{IEI, {info_element, {IEI, IEILen, IEIData}}} | Rest], Result) ->
+    udh_to_service(Rest, [IEI, IEILen, IEIData | Result]).
+
+%%--------------------------------------------------------------------
+%% Calculate UDH size
+%%--------------------------------------------------------------------
+get_udh_size(L) ->
+    get_udh_size(L, 0).
+
+get_udh_size([], Size) ->
+    case Size of
+        0 -> 0;
+        _ -> Size + 1 % +1 for UDHL field
+    end;
+get_udh_size([{_IEI, {info_element, {_IEI, IEILen, _IEIData}}} | Rest], Size) ->
+    get_udh_size(Rest, Size + 2 + IEILen).
 
 %%--------------------------------------------------------------------
 %% Create UCP 51 message
@@ -165,6 +318,9 @@ check_cmd_5x_options(Rec, []) ->
 check_cmd_5x_options(Rec, [{notification_request, Bool}|T])
   when Bool == true ->
     check_cmd_5x_options(Rec#ucp_cmd_5x{nrq = "1"}, T);
+check_cmd_5x_options(Rec, [{notification_type, Type}|T])
+  when is_integer(Type); Type =< 0; Type =< 7 ->
+    check_cmd_5x_options(Rec#ucp_cmd_5x{nt = integer_to_list(Type)}, T);
 check_cmd_5x_options(Rec, [{extra_services, Value}|T])
   when is_binary(Value) ->
     {ok, HexXser} = binpp:convert(Value),
@@ -176,6 +332,7 @@ check_cmd_5x_options(Rec, [{deferred_delivery_time, Value}|T]) % DDMMYYHHmm
   when is_list(Value); length(Value) =:= 10 ->
     check_cmd_5x_options(Rec#ucp_cmd_5x{dd = "1", ddt = Value}, T);
 check_cmd_5x_options(Rec, [_H|T]) ->
+    % TODO: return error
     % Option unknown or incorrect value: ~w", [H]).
     check_cmd_5x_options(Rec, T).
 
@@ -221,5 +378,4 @@ create_ack(Header) when is_record(Header, ucp_header) ->
 analyze_message(Message) ->
     Bit = unicode:bin_is_7bit(unicode:characters_to_binary(Message)),
     {'7bit', Bit}.
-
 
