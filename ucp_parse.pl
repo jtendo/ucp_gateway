8c62f174f4d5bf681daa11c7770a9e0498707570
Lepiocha <butcherek@gmail.com>
2011-11-16 10:33:39 +0100

    Remove unused file

--- ucp_parse.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl -w
-use Net::UCP;
-use Data::Dumper;
-
-$ucp = Net::UCP->new(FAKE => 1);
-my $smsc_message = $ARGV[0];
-$ref_msg = $ucp->parse_message($smsc_message);
-print Dumper($ref_msg);
